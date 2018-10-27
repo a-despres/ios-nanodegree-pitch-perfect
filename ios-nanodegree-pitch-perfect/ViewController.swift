ac7db2ec99ebf1e0c4babd768294cfff30a422a4
Andrew Despres <21027320+a-despres@users.noreply.github.com>
2018-10-26 17:03:48 -0700

    Lesson 2 Complete
    Lesson 2 - AutoLayout and Buttons
    In lesson 2 we added 2 buttons: "Record" and "Stop Recording" to Main.storyboard, as well as a label "Tap to Record." The two buttons are linked to IBActions that print a string to the console, signaling that the button was pressed. IBAction recordAudio(_:) also modifies the text of the label "Tap to Record" to "Recording in Progress" which is linked to IBOutlet "recordingLabel." Each of the buttons and the label have constraints applied so that the view stays constant on both an iPhone as well as an iPad in both portrait and landscape orientations.

--- ios-nanodegree-pitch-perfect/ViewController.swift
+++ ios-nanodegree-pitch-perfect/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Interface Builder Outlets
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    // MARK: - Interface Builder Actions
+    @IBAction func recordAudio(_ sender: UIButton) {
+        print("record button was pressed")
+        recordingLabel.text = "Recording in Progress"
+    }
+    @IBAction func stopRecording(_ sender: UIButton) {
+        print("stop recording button was pressed")
+    }
+    
+    // MARK: - UIViewController
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
